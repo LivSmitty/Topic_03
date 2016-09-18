0718b900e8e7fad27448f02f9c0c643ae160c27f
LivSmitty <livia@jansenresearch.com>
2016-09-18 10:40:06 -0700

    Commit to GIT

--- TimeConversion_2.java
+++ TimeConversion_2.java
@@ -1,18 +1,24 @@
+import java.util.Scanner;
+
 /**
  * Created by scott_000 on 9/15/2016.
  */
 
 public class TimeConversion_2 {
     public static void main(String[] args) {
-        String string = "4.50";
+        //Get time (in decimal format) from user
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a time in decimal format (i.e. 4.50): ");
+        String string = input.nextLine();
+
+        //Split the decimal at the period, assign each string a variable name.
         String[] parts = string.split("\\.");
         String part1 = parts[0];
         String part2 = parts[1];
-        System.out.println(parts[0]);
-        System.out.println(parts[1]);
 
+        //Convert time to minutes
         int minutes = (Integer.parseInt(parts[1]));
-        System.out.println(parts[0] + " hours and " + ((minutes * 6)/10) + " minutes.");
+        System.out.println("The time in minutes is: " + parts[0] + " hours and " + ((minutes * 6)/10) + " minutes.");
 
 
 
